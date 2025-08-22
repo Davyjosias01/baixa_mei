fe925e03ace126d7f3b290baab096ae0c2ea6bc7
davy <davy.josias@gmail.com>
2025-08-22 17:33:45 -0300

    Finalizando a sexta, continuo em casa

--- src/services/requests/get-certificate.service.js
+++ src/services/requests/get-certificate.service.js
@@ -1,34 +1,33 @@
-const { API_AUTHORIZATION } = require('../../config/env');
-const { CERTIFICATES_URL } = require('../../utils/urls');
-const { DATE_END_CERTIFICATE } = require('../../utils/dates');
+const { API_AUTHORIZATION }     = require('../../config/env');
+const { CERTIFICATES_URL }      = require('../../utils/urls');
+const { DATE_END_CERTIFICATE }  = require('../../utils/dates');
+const { getBuffer }             = require('../../utils/http')
+const fs                        = require('node:fs/promises');
+const path                      = require('node:path')
 
 
-
-async function getCertificate({ certificateType = 'A1', cnpj } = {}) {
+async function getCertificate({ certificateType = 'E-CNPJ A1', has_procuration = '', cnpj, destPath, } = {}) {
     if(!CERTIFICATES_URL) throw new Error("Não foi encontrada a variável CERTIFICATES_URL no arquivo .env!");
     if(!API_AUTHORIZATION) throw new Error("Não foi encontrada a variável API_AUTHORIZATION no arquivo .env!");
     
-    const url = new URL(process.env.CERTIFICATES_URL);
+    const url = new URL(CERTIFICATES_URL);
     url.searchParams.set('cnpj', cnpj)
     url.searchParams.set('date_start', '01/01/2000');
     url.searchParams.set('date_end', DATE_END_CERTIFICATE);
-    url.searchParams.set()
-    
+    url.searchParams.set('certificate_type', certificateType);
+    url.searchParams.set('has_procuration', has_procuration);
 
-    
-
-
-
-
+    const buffer = await getBuffer({ url, headers: { Authorization: API_AUTHORIZATION } });
+    await fs.mkdir(path.dirname(destPath), { recursive: true }).catch(() => {});
+    await fs.writeFile(destPath, buffer);
+    return destPath;
 }
-
-
 
 
 if (require.main === module) {
     ( async () => {
         try {
-            const data = getCertificate({});
+            const data = await getCertificate({cnpj: '46752607000129', destPath: 'C:\\Certificates\\certificado_importado.pfx'});
             console.log(data);
         } catch (e) {
             console.log(e);
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,18 +1,16 @@
 async function getJson({url, headers = {}} = {}) {
-    console.log(url)
+    console.log(url);
     const res = await fetch(url, {headers, method:'GET'});
     if (!res.ok) throw new Error(`GET ${url} -> ${res.status} ${res.statusText}`);
     return res.json();
 }
 
-async function getBuffer(url, headers = {}) {
+async function getBuffer({url, headers = {}} = {}) {
     const res = await fetch(url, {headers, method: 'GET'});
     if (!res.ok) throw new Error(`GET ${url} -> ${res.status} ${res.statusText}`);
     const ab = await res.arrayBuffer();
     return Buffer.from(ab);
 }
-
-
 
 
 module.exports = { getJson, getBuffer };
