8838c3038239185788f58f587983479bd26d8191
davy <davy.josias@gmail.com>
2025-08-28 14:05:45 -0300

    Ajustes para apresentar message box, configuração de url pós login no gov

--- src/jobs/baixa-mei/index.js
+++ src/jobs/baixa-mei/index.js
@@ -1,14 +1,19 @@
 const { chromium } = require('playwright');
-const { getServices } = require('../../services/requests/get-services');
-const { installCertificate } = require('../../services/install-certificate');
-const { getCertificate } = require('../../services/requests/get-certificate');
-const { getCompany } = require('../../services/requests/get-company');
-const { CUSTOMER_PORTAL_URL } = require('../../utils/urls');
+const { createInterface } = require('node:readline/promises');
 
+const { getCompany } =          require('../../services/requests/get-company');
+const { getServices } =         require('../../services/requests/get-services');
+const { getCertificate } =      require('../../services/requests/get-certificate');
+const { installCertificate } =  require('../../services/install-certificate');
+const { CUSTOMER_PORTAL_URL, JA_SOU_MEI_URL } = require('../../utils/urls');
+const { SELECTORS } = require('../../utils/selectors');
+const { execFileSync } = require('node:child_process');
 
 async function run() {
     //Request para capturar as obrigações
     let data, services;
+
+    const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));    
 
     try {
         data = await getServices({date_start:'01/08/2025', date_end:'01/08/2026'});
@@ -32,6 +37,7 @@ async function run() {
         if(company){
             console.log(`Iniciando o processo de baixa da empresa: ${company.id} - CNPJ: ` + company.cnpj);
 
+            /*
             //Buscando informações sobre o certificado
             try {
                 certificate = await getCertificate({cnpj: company.cnpj, destPath: certificateAddress});
@@ -41,8 +47,7 @@ async function run() {
                 console.log(e);
                 continue;
             }
-
-
+            
             //Instalar o certificado
             try {
                 await installCertificate({ pfxPath: certificateAddress, pfxPwd: certificate.digital_certificate_password});
@@ -50,28 +55,64 @@ async function run() {
                 console.log(e);
                 continue;
             }
-
+            */
 
             //Processo no site do MEI 
-            const browser = await chromium.launch({headless: false, args: ['--start-maximized']});
+            const browser = await chromium.launch({channel: 'chrome', headless: false, args: ['--start-maximized']});
             const context = await browser.newContext({viewport: null});
             const page = await context.newPage();
 
 
             await page.goto(CUSTOMER_PORTAL_URL, { waitUntil: 'networkidle', timeout: 60000 });
-
+            await page.locator(SELECTORS.BTN_LOGIN_NO_GOV).click();
+            await page.locator(SELECTORS.BTN_SEU_CERTIFICADO_DIGITAL).waitFor({ state: 'visible', timeout: 20000 });
             
+            await waitHumanBox({ message: 'Faça o login no gov de acordo com as credenciais do cliente.' });
+
+            await page.goto(JA_SOU_MEI_URL, { waitUntil: 'networkidle', timeout: 60000 });
+            
+
+
+
+
+/*
+            await loc.waitFor({ state: 'visible', timeout:  16000 });
+            await loc.scrollIntoViewIfNeeded();
+            await loc.click({ timeout });
+*/
             await browser.close();
-
-
         }
     }
 }
 
-    
+function psEscape(s = ''){return String(s).replace(/'/g, "''");}
+
+async function waitHumanBox({ message, title = 'Caixa de espera', buttons = 'OKCancel', icon = 'Information' }={}) {
+    const ps = `
+Add-Type -AssemblyName System.Windows.Forms
+$btns = [System.Windows.Forms.MessageBoxButtons]::${buttons}
+$ico  = [System.Windows.Forms.MessageBoxIcon]::${icon}
+$r = [System.Windows.Forms.MessageBox]::Show('${psEscape(message)}','${psEscape(title)}',$btns,$ico)
+if ($r -eq [System.Windows.Forms.DialogResult]::OK) { exit 0 } else { exit 1 }
+`;
+  try {
+    execFileSync('powershell.exe', ['-NoProfile','-Sta','-ExecutionPolicy','Bypass','-Command', ps], { stdio: 'inherit' });
+        return true;
+    } catch (e) {
+        console.log(e)
+        return false;
+    }
+}
+
+async function waitHumanTerminal(msg = 'Conclua a ação e pressione Enter...') {
+    const rl = createInterface({ input: process.stdin, output: process.stdout });
+    await rl.question(`${msg}\n`);
+    rl.close();
+}
 
 module.exports = { run };
 
+//Se for requerido o node .\index.js
 if (require.main === module){
     (async () => {
         await run();
